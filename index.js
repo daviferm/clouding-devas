5f910cb4b99cdf18ddab99248d057dda62b1a862
David Fernández <dfernandez.martin@hotmail.com>
2025-05-18 19:15:17 +0200

    puerto 3049

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
 const app = express();
-const PORT = 2083;
+const PORT = 3049;
 
 
 //** Configurar los cors
