5badad9f61b1a396c56d49fc13f928b7a018ad37
David Fernández <dfernandez.martin@hotmail.com>
2025-05-18 19:17:31 +0200

    puerto 8447

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
 const app = express();
-const PORT = 3049;
+const PORT = 8447;
 
 
 //** Configurar los cors
