47d2a96b7f154db9f4516c87b051d86ad7cd283c
David Fernández <dfernandez.martin@hotmail.com>
2025-05-18 17:42:20 +0200

    puerto 443

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
 const app = express();
-const PORT = 3049;
+const PORT = 443;
 
 
 //** Configurar los cors
